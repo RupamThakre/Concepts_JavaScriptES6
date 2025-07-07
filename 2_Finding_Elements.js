1d6580c65ff7a0b0a14412649aab5be9485f73d6
RupamThakre <rupamthakre15@gmail.com>
2025-07-07 11:05:15 +0530

    Finding Elements Arrays

---
+++ 2_Finding_Elements.js
@@ -0,0 +1,16 @@
+//[1]   .indexOf()
+//[2]   .lastIndexOf()
+//[3]   .includes()
+
+// numbers.indexOf(1) !== -1
+// numbers.includes(1);
+
+const numbers = [1,2,3,1,4];
+
+console.log(numbers.indexOf(3)); //2
+
+console.log(numbers.lastIndexOf(1)); //3
+
+console.log(numbers.indexOf(1) !== -1); //true
+
+console.log(numbers.includes(1)); //true
